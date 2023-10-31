811646151a56e07f8f221b8732ff6fc667861910
keverni <65460632+keverni@users.noreply.github.com>
2023-10-31 14:46:40 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,25 +3,41 @@ tg.expand();
 
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#2cab37";
-let a =[
-    new Date(new Date().getFullYear(),new Date().getMonth(),0).getDate(),
-    new Date(new Date().getFullYear(),new Date().getMonth() + 1,0).getDate(),
-    new Date(new Date().getFullYear(),new Date().getMonth() + 2,1).getDate(),
-    new Date(new Date().getFullYear(),new Date().getMonth() + 3,2).getDate(),
-    new Date(new Date().getFullYear(),new Date().getMonth() + 4,3).getDate(),
-    new Date(new Date().getFullYear(),new Date().getMonth() + 5,4).getDate(),
-    new Date(new Date().getFullYear(),new Date().getMonth() + 6,5).getDate()
-];
-D1 = new Date(new Date().getFullYear(),new Date().getMonth(),a[0]);
-D2 = new Date(new Date().getFullYear(),new Date().getMonth(),a[1]);
-D3 = new Date(new Date().getFullYear(),new Date().getMonth(),a[2]);
-D4 = new Date(new Date().getFullYear(),new Date().getMonth(),a[3]);
-D5 = new Date(new Date().getFullYear(),new Date().getMonth(),a[4]);
-D6 = new Date(new Date().getFullYear(),new Date().getMonth(),a[5]);
-for (let i = 0; i < a.length; ++i) {
-    console.log(new Date(new Date().getFullYear(),new Date().getMonth(),a[i]));
+let week_tr = "<tr>";
+let dict = {
+    1 : "ПН",
+    2 : "ВТ",
+    3 : "СР",
+    4 : "ЧТ",
+    5 : "ПТ",
+    6 : "СБ",
+    0 : "ВС",
 }
+let week =[
+    new Date(new Date().getFullYear(),new Date().getMonth(),0).getDate() + 1,
+    new Date(new Date().getFullYear(),new Date().getMonth(),0).getDate() + 2,
+    new Date(new Date().getFullYear(),new Date().getMonth(),0).getDate() + 3,
+    new Date(new Date().getFullYear(),new Date().getMonth(),0).getDate() + 4,
+    new Date(new Date().getFullYear(),new Date().getMonth(),0).getDate() + 5,
+    new Date(new Date().getFullYear(),new Date().getMonth(),0).getDate() + 6,
+    new Date(new Date().getFullYear(),new Date().getMonth(),0).getDate() + 7
+];
 
+for (let i = 0; i < week.length; ++i)
+{
+
+    D = new Date(new Date().getFullYear(),new Date().getMonth(),week[i]);
+
+    week_tr += "<td>" + dict[D.getDay()] + "</td>";
+}
+document.getElementById("table_cal").innerHTML = week_tr;
+
+function Setting() {
+    location.href = "Settings.html";
+}
+function GoBack() {
+    location.href = "index.html";
+}
 
 
 
